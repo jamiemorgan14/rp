dbc890bd54ecd8dcbe5242bedcd38b3f274aa873
Jamie Morgan <jamiemorgan@truckstop.com>
2020-03-20 11:33:02 -0600

    initial

---
+++ restaurant-picker/restaurant-picker.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// export interface Props {
+//     name: string;
+//     number: number;
+// }
+
+export const RestaurantPicker: React.FC = props => {
+    return (
+        <>
+        <h1>{this.props.greeting}</h1>
+        </>)
+}
